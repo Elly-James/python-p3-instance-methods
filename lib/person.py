39aac1b9bdd3934a0c4fec9d3906dab6fbe5115e
Elly <ellyjames1999@gmail.com>
2025-02-14 09:21:33 +0300

    done all the tests

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,18 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+
+person = Person()
+person.talk()  # Output: Hello, I am a person.
+
+
+person.walk()  # Output: The person is walking.
+
+
